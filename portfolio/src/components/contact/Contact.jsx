7a8d01554875638ff9164cfb8f7daca2a286d66f
Githubmagali <magalivictoria85068@gmail.com>
2024-01-12 15:13:53 -0300

    12-01-2024

---
+++ portfolio/src/components/contact/Contact.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './contact.css';
+
+
+
+const Contact =()=>{
+    return(<>
+    <section className="contact" id="contact">
+        <form action="">
+            <input type=""></input>
+        </form>
+    </section>
+    </>
+    );
+}
+
+export default Contact;
